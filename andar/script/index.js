67a8c84528e9adcc9541183c6b1b43171479917f
alsn8539 <alsn8539@gmail.com>
2025-02-05 00:28:36 +0900

    페이지네이션(작업중)

---
+++ andar/script/index.js
@@ -0,0 +1,13 @@
+const mainSwiper = new Swiper('.swiper',{
+    /* effect:'fade',  */// 제자리에서 배너 이미지 변경하는 스와이퍼 효과
+    autoplay:{
+        delay:4000, // 실제 배너 5~7초 많이 사용
+    },
+    loop:true,
+    pagination:{
+        el:'.main_swiper .swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        dynamicBullets:true, //페이지 번호 크기 차이 주는 속성
+    },
+});
