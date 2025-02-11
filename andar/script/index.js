af9287786bec11f30727a5660e3c75dd67761925
alsn8539 <alsn8539@gmail.com>
2025-02-11 23:24:51 +0900

    안다르(진짜 js 해야함..)

--- andar/script/index.js
+++ andar/script/index.js
@@ -1,3 +1,9 @@
+// const aClick = document.querySelectorAll('nav a');
+// console.log(aClick)
+// aClick.addEventListener('click',(e) => {
+//     
+// })
+
 const mainSwiper = new Swiper('.main_swiper',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
@@ -11,45 +17,48 @@ const mainSwiper = new Swiper('.main_swiper',{
     },
 });
 
-
+// 2행 에어쿨링
 const contentSwiper = new Swiper('.c_swiper',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
     },
     loop:true,
     scrollbar: {
-        el: " .progress .swiper-scrollbar",
+        el: " .row2 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row2 .s_btn .next",
+        prevEl: ".row2 .s_btn .prev",
     }
 });
 
+// 3행 신상
 const contentSwiper2 = new Swiper('.c_swiper2',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
     },
     loop:true,
     scrollbar: {
-        el: " .progress .swiper-scrollbar",
+        el: " .row3 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row3 .s_btn .next",
+        prevEl: ".row3 .s_btn .prev",
     }
 });
 
+//베스트셀러
 const contentSwiper3 = new Swiper('.c_swiper3',{
     autoplay:{
         delay:5000, // 실제 배너 5~7초 많이 사용
     },
     loop:true,
     scrollbar: {
-        el: " .progress .swiper-scrollbar",
+        el: " .row4 .progress .swiper-scrollbar",
     },
     navigation: {
-        nextEl: ".s_btn .next",
-        prevEl: ".s_btn .prev",
+        nextEl: ".row4 .s_btn .next",
+        prevEl: ".row4 .s_btn .prev",
     }
 });
+
