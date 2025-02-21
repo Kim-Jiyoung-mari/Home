18c59b06f2b6f73181734cf7962ffb6f5a10cd5f
alsn8539 <alsn8539@gmail.com>
2025-02-21 23:56:29 +0900

    플리토 집(백업업)

---
+++ flito-main/script/index.js
@@ -0,0 +1,22 @@
+// 메인 스와이퍼
+const mainS = new Swiper('#main',{
+    mousewheel:true,
+    direction:'vertical',
+    behavior:'smooth',
+    on:{
+        slideChangeTransitionEnd:function(){
+            setTimeout(()=> ScrollTrigger.refresh() ,0)
+        }
+    },
+});
+
+// 위로 버튼 클릭시 스크롤 이동시키기
+const topBtn = document.querySelector('.top');
+const slide = document.querySelectorAll('.swiper-slide');
+console.log(topBtn,slide);
+
+topBtn.addEventListener('click',()=>{
+    mainS.slideTo(0, 1000);
+});
+
+// 카운트 효과
