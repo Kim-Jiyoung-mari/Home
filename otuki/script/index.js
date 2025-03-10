c4ec2aaa5deeae1e3cafb244865b227f08627ea8
alsn8539 <alsn8539@gmail.com>
2025-03-10 22:58:26 +0900

    오뚜기 메인 배너

---
+++ otuki/script/index.js
@@ -0,0 +1,10 @@
+//메인배너
+const mainBnr = new Swiper('.main_bnr',{
+    // autoplay:{
+    //     delay:5000, // 실제 배너 5~7초 많이 사용
+    // },
+    loop:true,
+    scrollbar: {
+        el: ".main_bnr .swiper-scrollbar",
+    },
+});
